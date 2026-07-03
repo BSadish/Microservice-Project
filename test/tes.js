35ea9352e6912e5a3e30ec00117e8345f9295328
sadish panta <sadishpanta404@gmail.com>
2026-07-03 21:39:30 +0545

    load testing code

---
+++ test/tes.js
@@ -0,0 +1,17 @@
+const autocannon=require("autocannon");
+
+const url='http://localhost:8000';
+const duration = 30;
+
+const instance=autocannon({
+    url,
+    duration
+},(err,result)=>{
+    if(err){
+        console.log("Error", err);
+    } else{
+        console.log("Result:", result)
+    }
+})
+
+autocannon.track(instance);
