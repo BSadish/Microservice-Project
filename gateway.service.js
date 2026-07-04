abdd8ece56c20fe3a1df4affba6f5af283afca1d
sadish panta <sadishpanta404@gmail.com>
2026-07-04 19:18:57 +0545

    init API gateway with express-http-proxy

--- gateway.service.js
+++ gateway.service.js
@@ -3,5 +3,9 @@ import proxy from "express-http-proxy";
 
 const app=express();
 
-app.use('/stress-test',proxy('http:/localhost:8080'))
-app.use('/',proxy('http:/localhost:8080'))
+app.use('/stress-test',proxy('http:/localhost:8000'))
+// app.use('/',proxy('http:/localhost:8080'))
+
+app.listen(process.env.PORT ||8080, ()=>{
+    console.log("Gateway service is running on port ", process.env.PORT)
+})
